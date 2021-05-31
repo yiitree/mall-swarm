a134b74b9ace7070eaebcd38c212907988e2b0cb
zengrui <772701414@qq.com>
2021-05-31 13:30:07 +0800

    init

--- mall-demo/src/main/java/com/macro/mall/demo/component/FeignRequestInterceptor.java
+++ mall-demo/src/main/java/com/macro/mall/demo/component/FeignRequestInterceptor.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 
 /**
- * 用于Feign传递请求头的拦截器
+ * 用于Feign传递请求头的拦截器 -
  * Created by macro on 2019/10/18.
  */
 public class FeignRequestInterceptor implements RequestInterceptor {
